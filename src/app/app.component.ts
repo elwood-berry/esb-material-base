8617f8a77e2af7836039e1945674a4e5ede72d79
elwood-berry <eberry@anterasoftware.com>
2021-07-01 10:22:30 -0500

    5 files modified, 4 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+/*
+DEMO 
+*/
+import { Component, VERSION, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 
+//
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
 
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+// CLASS
+export class AppComponent {
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+
+  public panelOpenState = false;
 }
---
+++ src/app/components/base-expansion/base-expansion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-expansion',
+  templateUrl: './base-expansion.component.html',
+  styleUrls: ['./base-expansion.component.css']
+})
+export class BaseExpansionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
