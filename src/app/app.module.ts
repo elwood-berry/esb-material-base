9205dd5e19c17fda6caf47022ebca16d4d03edfb
elwood-berry <eberry@anterasoftware.com>
2021-07-01 10:32:55 -0500

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -63,6 +63,7 @@ import { AppComponent } from './app.component'; // 1. APP
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     A11yModule,
     ClipboardModule,
